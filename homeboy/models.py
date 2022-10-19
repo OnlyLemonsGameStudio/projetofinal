6d060fed8c54fb37c7d2ae8479e74b87835252e4
OnlyLemonsGameStudio <onlylemons.gamestudio@gmail.com>
2022-10-18 21:29:10 -0300

    Criação do model Produto

--- homeboy/models.py
+++ homeboy/models.py
@@ -1,3 +1,7 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    produto_nome = models.CharField(max_length=200)
+    produto_preco = models.DecimalField(max_digits=6, decimal_places=2)
